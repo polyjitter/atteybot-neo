e0d8953e58f74dda19f14cbecc5644978c233559
taciturasa <polyjitter@fastmail.com>
2020-10-18 00:56:52 +0000

    Add more default plug work

--- extensions/plugs/default.py
+++ extensions/plugs/default.py
@@ -22,10 +22,26 @@ class DefaultPlug():
     def members(self, ctx):
         return "Add or remove members."
 
-    @members.selection(emoji="plus")
+    @members.selection(emoji="plus", type="entry")
     def add_members(self, ctx, message, reaction):
         ...
 
-    @members.selection(emoji="minus")
+    @members.selection(emoji="minus", type="entry")
     def remove_members(self, ctx, message, reaction):
+        ...
+
+    @plugging.entry()
+    def access(self, ctx):
+        return "Set the access level of this room."
+
+    @access.selection(emoji="open_lock", type="exclusive")
+    def open_room(self, ctx, message, reaction):
+        ...
+
+    @access.selection(emoji="closed_lock", type="exclusive")
+    def locked_room(self, ctx, message, reaction):
+        ...
+
+    @access.selection(emoji="no_view", type="exclusive")
+    def unlisted_room(self, ctx, message, reaction):
         ...
